868c5e28eb495b6d28ff1c9284e634982b70912e
alexandrsergeev <sergeevbass@gmail.com>
2023-08-28 14:24:49 +0500

    Little fix.

--- src/main/java/com/rain/springbootdemo/controller/UserController.java
+++ src/main/java/com/rain/springbootdemo/controller/UserController.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
@@ -24,34 +25,39 @@ public class UserController {
     public String findAll(Model model) {
         List<User> listUsers = userService.findAll();
         model.addAttribute("users", listUsers);
-        return "user-list";                                          // здесь возвращаем строку, где будет находится логика
+        return "user-list";
     }
 
+    //    Create method
     @GetMapping("/user-create")
     public String createUserForm(User user) {           // этот метод отдельно создает форму, куда мы будем вбивать данные
         return "user-create";
     }
 
     @PostMapping("/user-create")
-    public String createUser(User user) {               // а вот здесь уже будем сохранять данные в базу
+    public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
-    @GetMapping("user-delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id){
+
+    //    Delete method
+    @GetMapping("/user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteById(id);
         return "redirect:/users";
     }
 
+
+    //    Update method
     @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model){
+    public String updateUserForm(@PathVariable("id") Long id, Model model) {
         User user = userService.findById(id);
         model.addAttribute("user", user);
         return "user-update";
     }
 
     @PostMapping("/user-update")
-    public String updateUser(User user){
+    public String updateUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
